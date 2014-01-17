5a3617a74d6b1d95a8a4952ec761344a7ba99dc7
rakeshreddyvanga <rakeshreddyvanga@gmail.com>
2014-01-17 02:20:54 -0500

    MVC-Demo

---
+++ cSharp/MvcApplication1/MvcApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Welcome(String name,int id)
+        {
+            return HttpUtility.HtmlEncode("Hello "+name+", Welcome to the class your id is:"+id);
+        }
+    }
+}
